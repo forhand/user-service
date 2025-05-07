0f5058b506980b3d1fa6a4be409b7345db31568b
serg <shkaev.ss@mail.ru>
2025-05-07 22:09:43 +0300

    update(User) add fields: email, followers and followees

--- src/main/java/org/example/user_service/entity/User.java
+++ src/main/java/org/example/user_service/entity/User.java
@@ -5,11 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -25,8 +30,17 @@ public class User {
   private String username;
   @Column(name = "password", length = 128, nullable = false)
   private String password;
+  @Column(name = "email", length = 64, nullable = false, unique = true)
+  private String email;
   @Column(name = "active", nullable = false)
   private boolean active;
+  @ManyToMany
+  @JoinTable(name = "subscriptions",
+          joinColumns = @JoinColumn(name = "followee_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+  private List<User> followers;
+
+  @ManyToMany(mappedBy = "followers")
+  private List<User> followees;
 
 
 }
