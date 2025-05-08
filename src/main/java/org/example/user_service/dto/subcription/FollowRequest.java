45fae62d9169b07cb24f43b7649d883f70004a32
serg <shkaev.ss@mail.ru>
2025-05-08 19:24:42 +0300

    create(FollowRequest) pojo FollowRequest

---
+++ src/main/java/org/example/user_service/dto/subcription/FollowRequest.java
@@ -0,0 +1,20 @@
+package org.example.user_service.dto.subcription;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class FollowRequest {
+
+  @NotNull
+  @Positive(message = "FollowerId must be positive")
+  private Long followerId;
+  @NotNull
+  @Positive(message = "FolloweeId must be positive")
+  private Long followeeId;
+}
