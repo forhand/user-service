e403c938291e4052f8c41af248852a29ae6e7e24
serg <shkaev.ss@mail.ru>
2025-05-08 13:21:18 +0300

    update(UserMapper) add tests
    - use UserDataContainer
    - toDto_WOFollowees
    - toDto_WOFollowers

--- src/test/java/org/example/user_service/mapper/UserMapperTest.java
+++ src/test/java/org/example/user_service/mapper/UserMapperTest.java
@@ -2,60 +2,71 @@ package org.example.user_service.mapper;
 
 import org.example.user_service.dto.userDto.UserDto;
 import org.example.user_service.entity.User;
+import org.example.user_service.util.container.UserDataContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserMapperTest {
-    private UserMapperImpl userMapper = new UserMapperImpl();
-    User user;
-    UserDto userDto;
+  private UserMapperImpl userMapper = new UserMapperImpl();
+  private UserDataContainer container;
+  User user;
+  UserDto userDto;
 
-    @BeforeEach
-    void setUp() {
-        Long userId = 1L;
-        String username = "username";
-        String password = "password";
-        boolean active = true;
-
-        user = User.builder()
-                .id(userId)
-                .username(username)
-                .password(password)
-                .active(active)
-                .build();
-
-        userDto = UserDto.builder()
-                .id(userId)
-                .username(username)
-                .password(password)
-                .active(active)
-                .build();
-    }
+  @BeforeEach
+  void setUp() {
+    container = new UserDataContainer();
+    user = container.getUser();
+    userDto = container.getUserDto();
+  }
 
 
-    @Test
-    void toDto() {
-        UserDto dto = userMapper.toDto(user);
+  @Test
+  void toDto() {
+    UserDto dto = userMapper.toDto(user);
 
-        assertEquals(userDto, dto);
-    }
+    assertEquals(userDto, dto);
+  }
 
-    @Test
-    void toEntity() {
-        User entity = userMapper.toEntity(userDto);
+  @Test
+  void toDto_WOFollowers() {
+    user.setFollowers(null);
+    userDto.setFollowerIds(new ArrayList<>());
 
-        assertEquals(user, entity);
-    }
+    UserDto actDto = userMapper.toDto(user);
 
-    @Test
-    void toEntityWOUserId() {
-        userDto.setId(null);
-        user.setId(null);
+    assertEquals(userDto, actDto);
+  }
 
-        User entity = userMapper.toEntity(userDto);
+  @Test
+  void toDto_WOFollowees() {
+    user.setFollowees(null);
+    userDto.setFolloweeIds(new ArrayList<>());
 
-        assertEquals(user, entity);
-    }
+    UserDto actDto = userMapper.toDto(user);
+
+    assertEquals(userDto, actDto);
+  }
+
+  @Test
+  void toEntity() {
+    user.setFollowers(null);
+    user.setFollowees(null);
+    User entity = userMapper.toEntity(userDto);
+
+    assertEquals(user, entity);
+  }
+
+  @Test
+  void toEntity_WOUserId() {
+    userDto.setId(null);
+    user.setId(null);
+
+    User entity = userMapper.toEntity(userDto);
+
+    assertEquals(user.getId(), entity.getId());
+  }
 }
