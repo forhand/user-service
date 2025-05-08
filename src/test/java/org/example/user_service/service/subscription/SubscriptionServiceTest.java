a502957a40377f16a235d7374c019eaf4abd2433
serg <shkaev.ss@mail.ru>
2025-05-08 21:04:44 +0300

    update(SubscriptionServiceTest) tests
    - followUser
    - unfollowUser

---
+++ src/test/java/org/example/user_service/service/subscription/SubscriptionServiceTest.java
@@ -0,0 +1,77 @@
+package org.example.user_service.service.subscription;
+
+import org.example.user_service.handler.exception.DataValidationException;
+import org.example.user_service.handler.exception.ResourceNotFoundException;
+import org.example.user_service.repository.SubscriptionRepository;
+import org.example.user_service.service.UserService;
+import org.example.user_service.util.container.SubscriptionDataContainer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doThrow;
+
+@ExtendWith(MockitoExtension.class)
+class SubscriptionServiceTest {
+  @Mock
+  private SubscriptionRepository subscriptionRepository;
+  @Mock
+  private UserService userService;
+  @Mock
+  private MessageSource messageSource;
+  @Mock
+  private SubscriptionDataValidator validator;
+  @InjectMocks
+  private SubscriptionService service;
+  private SubscriptionDataContainer subscriptionDataContainer;
+
+
+  @BeforeEach
+  void setUp() {
+    subscriptionDataContainer = new SubscriptionDataContainer();
+  }
+
+  @Test
+  void testFollowUser_successful() {
+    assertDoesNotThrow(() -> service.followUser(1L, 2L));
+  }
+
+  @Test
+  void testFollowUser_throwsDataValidationException() {
+    doThrow(new DataValidationException("Invalid input")).when(validator).validateFollowUser(anyLong(), anyLong());
+
+    assertThrows(DataValidationException.class, () -> service.followUser(1L, 2L));
+  }
+
+  @Test
+  void testFollowUser_throwsResourceNotFoundException() {
+    doThrow(new ResourceNotFoundException("User not found")).when(validator).validateFollowUser(anyLong(), anyLong());
+
+    assertThrows(ResourceNotFoundException.class, () -> service.followUser(1L, 2L));
+  }
+
+  @Test
+  void testUnfollowUser() {
+    assertDoesNotThrow(() -> service.unfollowUser(1L, 2L));
+  }
+
+  @Test
+  void testUnfollowUser_throwsDataValidationException() {
+    doThrow(new DataValidationException("Invalid input")).when(validator).validateUnfollowUser(anyLong(), anyLong());
+
+    assertThrows(DataValidationException.class, () -> service.unfollowUser(1L, 2L));
+  }
+
+  @Test
+  void testUnfollowUser_throwsResourceNotFoundException() {
+    doThrow(new ResourceNotFoundException("User not found")).when(validator).validateFollowUser(anyLong(), anyLong());
+
+    assertThrows(ResourceNotFoundException.class, () -> service.followUser(1L, 2L));
+  }
+}
