d0ef6b6bdfaab27437f7f753f59dd55fa04f3b50
serg <shkaev.ss@mail.ru>
2025-05-08 13:19:08 +0300

    update(UserMapper) add mapping
    - followerIds - follower
    - followeeIds - followee

--- src/main/java/org/example/user_service/mapper/UserMapper.java
+++ src/main/java/org/example/user_service/mapper/UserMapper.java
@@ -3,9 +3,28 @@ package org.example.user_service.mapper;
 import org.example.user_service.dto.userDto.UserDto;
 import org.example.user_service.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Collections;
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
-public interface UserMapper{
+public interface UserMapper {
+
+  @Mapping(target = "followerIds", expression = "java(mapToUserIds(entity.getFollowers()))")
+  @Mapping(target = "followeeIds", expression = "java(mapToUserIds(entity.getFollowees()))")
   UserDto toDto(User entity);
+
+  @Mapping(target = "followers", ignore = true)
+  @Mapping(target = "followees", ignore = true)
   User toEntity(UserDto dto);
+
+  default List<Long> mapToUserIds(List<User> users) {
+    if (users == null) {
+      return Collections.emptyList();
+    }
+    return users.stream()
+            .map(User::getId)
+            .toList();
+  }
 }
