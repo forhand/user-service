6aac183d226b8d02c2e1dc78182af0b15399441f
serg <shkaev.ss@mail.ru>
2025-05-08 13:23:45 +0300

    update(UserService) add MessageSource and method validateUserExists

--- src/main/java/org/example/user_service/service/UserService.java
+++ src/main/java/org/example/user_service/service/UserService.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.user_service.dto.userDto.UserDto;
 import org.example.user_service.entity.User;
-import org.example.user_service.handler.exception.UserNotFoundException;
+import org.example.user_service.handler.exception.ResourceNotFoundException;
 import org.example.user_service.mapper.UserMapper;
 import org.example.user_service.repository.UserRepository;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ import java.util.List;
 public class UserService {
   private final UserRepository userRepository;
   private final UserMapper userMapper;
+  private final MessageSource messageSource;
 
   public UserDto getUser(long userId) {
     User user = getUserById(userId);
@@ -38,7 +41,7 @@ public class UserService {
 
   public UserDto deactivateUserProfile(Long userId) {
     User user = getUserById(userId);
-    if(user.isActive()){
+    if (user.isActive()) {
       user.setActive(false);
       log.info("User {} deactivated", userId);
       // todo: опубликовать событие об изменении статуса аккаунта
@@ -49,7 +52,17 @@ public class UserService {
     return userMapper.toDto(user);
   }
 
+  public void validateUserExists(long userId) {
+    if (!userRepository.existsById(userId)) {
+      throw new ResourceNotFoundException(
+              messageSource.getMessage("user.not_found", new Object[]{userId}, Locale.getDefault())
+      );
+    }
+  }
+
   private User getUserById(long userId) {
-    return userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+    return userRepository.findById(userId).orElseThrow(
+            () -> new ResourceNotFoundException(messageSource.getMessage("user.not_found", new Object[]{userId}, Locale.getDefault())
+            ));
   }
 }
