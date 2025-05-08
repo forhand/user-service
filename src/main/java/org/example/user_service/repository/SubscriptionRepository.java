c8e14065b08a329b9c383b4d1e1727f10e2192d9
serg <shkaev.ss@mail.ru>
2025-05-08 19:31:54 +0300

    create(SubscriptionRepository) make followUser() and existsByFollowerIdAndFolloweeId()

---
+++ src/main/java/org/example/user_service/repository/SubscriptionRepository.java
@@ -0,0 +1,23 @@
+package org.example.user_service.repository;
+
+import org.example.user_service.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Stream;
+
+@Repository
+public interface SubscriptionRepository extends CrudRepository<User, Long> {
+
+  @Query(nativeQuery = true, value = "insert into subscriptions (follower_id, followee_id) values (:followerId, :followeeId)")
+  @Modifying
+  @Transactional
+  void followUser(long followerId, long followeeId);
+
+  @Query(nativeQuery = true, value = "select exists(select 1 from subscriptions where follower_id = :followerId and followee_id = :followeeId)")
+  boolean existsByFollowerIdAndFolloweeId(long followerId, long followeeId);
+  
+}
