eac42d318c2c19f8559f773a85e63f5543f81d35
serg <shkaev.ss@mail.ru>
2025-05-08 19:22:44 +0300

    update(ExceptionHandler) add MethodArgumentNotValidException

--- src/main/java/org/example/user_service/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/user_service/handler/GlobalExceptionHandler.java
@@ -5,11 +5,16 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.example.user_service.handler.exception.DataValidationException;
 import org.example.user_service.handler.exception.ResourceNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
@@ -17,10 +22,20 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(ConstraintViolationException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ErrorResponse handleException(ConstraintViolationException e, HttpServletRequest rq) {
+  public ErrorResponse handleConstraintViolationException(ConstraintViolationException e, HttpServletRequest rq) {
     log.error("Validation error: {}", e.getMessage(), e);
-    System.out.println(e.getMessage());
     return responseBuild(e.getMessage(), rq.getRequestURI());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest rq) {
+    BindingResult result = e.getBindingResult();
+    List<String> errorMessages = result.getFieldErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .toList();
+    log.error("Validation error: {}", e.getMessage(), e);
+    return responseBuild(errorMessages.toString(), rq.getRequestURI());
   }
 
   @ExceptionHandler(DataValidationException.class)
@@ -52,17 +67,17 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(RuntimeException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ErrorResponse handleRuntimeException(RuntimeException e, HttpServletRequest rq) {
     log.error("RuntimeException: {}", e.getMessage(), e);
-    return responseBuild(e.getMessage(), rq.getRequestURI());
+    return responseBuild("Server error. Everything will work soon, please try again later.", rq.getRequestURI());
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ErrorResponse handleException(Exception e, HttpServletRequest rq) {
     log.error("Exception: {}", e.getMessage(), e);
-    return responseBuild(e.getMessage(), rq.getRequestURI());
+    return responseBuild("Server error. Everything will work soon, please try again later.", rq.getRequestURI());
   }
 
   private ErrorResponse responseBuild(String message, String requestURI) {
